cbe6840d4680939b94129321af808927533f91f3
Noelia Sabando <lolia_15@hotmail.com>
2018-07-26 12:17:31 -0400

    arreglo indentacion

--- inicio.js
+++ inicio.js
@@ -22,26 +22,14 @@ boton.addEventListener("click", event=>{
 
 const renderInfo = datos => {
   listaRestaurant.innerHTML = "";
-  
-
-
   datos.restaurants.forEach((restaurant) => {
     listaRestaurant.innerHTML += `<div class="restaurant">
     <p>Nombre: ${restaurant.restaurant.name}</p>
     <p>Dirección: ${restaurant.restaurant.location.address}</p>
-    <a href="${restaurant.restaurant.events_url}">Visita la página</a>  
-    
+    <a href="${restaurant.restaurant.events_url}">Visita la página</a>      
     </div>`    
   })
 } 
  
 
 
-/* containerName.innerHTML += `<img src="${datos.restaurants["0"].restaurant.featured_image}">`; */
-/* const renderInfo= datos =>{
-  containerImage.innerHTML="";
-  containerName.innerHTML += datos.restaurants["0"].restaurant.featured_image;  
-  containerDireccion.innerHTML += datos.restaurants["0"].restaurant.location.address;  
-  containerUrl.innerHTML += datos.restaurants["0"].restaurant.menu_url;  
-  containerUrl.innerHTML += `<a href="${restaurant.restaurant.events_url}"></a> `
-} */
